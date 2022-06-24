c5659c7f332e394c1eac2a914aa8d823c3a1c79d
William Groc <william.groc@sogeti.com>
2022-06-24 17:04:59 +0200

    Init exo project

---
+++ src/store/todo/reducer.js
@@ -0,0 +1,12 @@
+const defaultData = {
+  todos: [],
+};
+
+function todoReducer(state = defaultData, { type, payload }) {
+  switch (type) {
+    default:
+      return state;
+  }
+}
+
+export default todoReducer;
